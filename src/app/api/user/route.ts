0bce3cbb70d24d2276fca7cc6db45e9f4829adde
Elalitareq <117711772+Elalitareq@users.noreply.github.com>
2023-06-10 03:30:18 +0300

    added something to it

--- src/app/api/user/route.ts
+++ src/app/api/user/route.ts
@@ -10,20 +10,25 @@ interface RequestBody {
 }
 
 export async function POST (request: Request){
-    const body: RequestBody =await request.json();
-    if(body.password !== body.confirmPassword){
-        return new Response(JSON.stringify({message: 'Password not match'}), {status: 401});    
-    }
-    const user = await prisma.user.create({
-        data:{
+    try{
 
-            name: body.name,
-            email: body.email,
-            password:bcrypt.hashSync(body.password, 10),
-            role: body.role
+        const body: RequestBody =await request.json();
+        if(body.password !== body.confirmPassword){
+            return new Response(JSON.stringify({message: 'Password not match'}), {status: 401});    
         }
-    });
-    const {password,...userWithoutPassword} = user;
-    return new Response(JSON.stringify(user), {status:201});
+        const user = await prisma.user.create({
+            data:{
+    
+                name: body.name,
+                email: body.email,
+                password:bcrypt.hashSync(body.password, 10),
+                role: body.role
+            }
+        });
+        const {password,...userWithoutPassword} = user;
+        return new Response(JSON.stringify(user), {status:201});
+    }catch(e){
+        return new Response(JSON.stringify({error: e}), {status: 500});
+    }
 
 }
