3c7993df87c194470427c5547bb10d47cbea8d5e
Elalitareq <117711772+Elalitareq@users.noreply.github.com>
2023-06-10 05:12:10 +0300

    ea

--- src/app/lib/prisma/index.ts
+++ src/app/lib/prisma/index.ts
@@ -6,9 +6,13 @@ declare global {
   var prisma: PrismaClient;
 }
 
+if (process.env.NODE_ENV === 'production') {
+  prisma = new PrismaClient();
+} else {
   if (!(global as any).prisma) {
     (global as any).prisma = new PrismaClient();
   }
   prisma = (global as any).prisma;
+}
 
 export default prisma;
