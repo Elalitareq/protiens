2d22505cfeb6e92727145af6d02f516a9e767f73
Elalitareq <elalitareq@gmail.com>
2023-08-05 01:33:31 +0300

    test

--- src/app/[product]/page.tsx
+++ src/app/[product]/page.tsx
@@ -10,13 +10,13 @@ const Product = ({ params }: { params: { product: String } }) => {
   const [product] = products;
   return (
     <>
-      <div className=" pt-24 pb-10 px-4 min-h-screen bg-new-black-green w-full flex flex-col md:flex-row items-center">
+      <div className=" pt-24 pb-10 px-4 min-h-screen bg-new-black-green w-full flex flex-col  items-center md:items-start">
         <Image
           src={productImages[product.id]}
           alt={product.name}
           width="300"
           height="500"
-          className="w-full rounded-lg"
+          className="w-full rounded-lg lg:w-[300px]"
         />
         <h2 className="text-2xl font-Roboto font-extrabold mt-6 mb-2">{product.name}</h2>
         <h4 className="my-3 text-gray-300 font-thin">
